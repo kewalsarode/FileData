17baf715f7ce9af4194dbe4f43b35c405547c1f7
Kewal Sarode <kewal.sarode@multichoice.co.za>
2021-08-26 19:30:20 +0530

    unit tests added

--- FileData.Test/FileDataTests.cs
+++ FileData.Test/FileDataTests.cs
@@ -16,5 +16,46 @@ namespace FileData.Test
 
             Assert.Matches("\\d+(\\.\\d+)+", version);
         }
+
+        [Theory]
+        [InlineData("-v")]
+        [InlineData("--v")]
+        [InlineData("/v")]
+        [InlineData("--version")]
+        public void VersionTestValidArgsV2(string indicator)
+        {
+            var fileProcessor = new Mock<IFileVersionProcessor>();
+
+            var version = fileProcessor.Object.GetVersion(indicator, "C:/test.txt");
+
+            Assert.Matches(@"\bv?[0-9]+\.[0-9]+\.[0-9]+(?:\.[0-9]+)?\b", version);
+        }
+
+        [Theory]
+        [InlineData("-s")]
+        [InlineData("--s")]
+        [InlineData("/s")]
+        [InlineData("--size")]
+        public void SizeTestValidArg(string indicator)
+        {
+            var fileProcessor = new Mock<IFileSizeProcessor>();
+
+            var size = fileProcessor.Object.GetSize(indicator, "C:/test.txt");
+
+            Assert.IsType(typeof(int), size);
+        }
+
+        [Theory]
+        [InlineData("s")]
+        [InlineData("sa")]
+        [InlineData("//s")]
+        [InlineData("---size")]
+        public void SizeTestInvalidArgs(string indicator)
+        {
+            var fileProcessor = new Mock<IFileSizeProcessor>();
+
+
+            Assert.Throws<ArgumentException>(fileProcessor.Object.GetSize(indicator, "C:/test.txt"));
+        }
     }
 }
